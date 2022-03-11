9c81f223911881efe42f534abc2adf69d7e31b54
Helna <hskaria@suyati.com>
2022-03-11 12:05:02 +0530

    first

---
+++ Models/studentDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class studentDbContext: DbContext
+    {
+        public studentDbContext():base("DefaultConnection")
+        {
+
+        }
+        public DbSet<student> studentTable { get; set; }
+    }
+
+
+}
